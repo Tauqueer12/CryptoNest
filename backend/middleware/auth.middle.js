d22dd323fb5cc347499f8b424513e337ed7dd4a5
Md Tauqueer Perwaiz <mdtauqueerperwaiz@gmail.com>
2026-07-16 13:13:46 +0530

    Remove server logs for security

--- backend/middleware/auth.middle.js
+++ backend/middleware/auth.middle.js
@@ -6,20 +6,19 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 const auth = async (req, res, next) => {
-  try {
+    try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, SECRET_KEY);
-        console.log(decoded);
         const user = await User.findOne({
             _id: decoded.userId,
         });
-        if(!user) throw new Error('please authenticate');
-        req.token = token ;
-        req.user = user ;
+        if (!user) throw new Error('please authenticate');
+        req.token = token;
+        req.user = user;
         next();
 
     } catch (e) {
-        res.status(401).send({error : e.message})
+        res.status(401).send({ error: e.message })
     }
 }
 
